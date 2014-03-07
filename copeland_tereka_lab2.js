ac4fe31e9853ec0cdaaa5272820f6f8de7cfe642
terekacopeland <starsxmexico@live.com>
2014-03-07 16:09:55 -0500

    Lab 2 Commits

---
+++ copeland_tereka_lab2.js
@@ -0,0 +1,35 @@
+alert("JavaScript works!");
+
+//SDI 1403
+//Lab 2
+//Variables and Output
+
+//My Variables
+var myWaiterName = "";
+var IamHungry = true;
+var numOfResturantChoices = 3;
+var askQuestion = true;
+var soupChoice = true;
+var soupChoiceF = false;
+var myWaiterName = prompt("What is your waiter's name?");
+
+
+//My Outputs
+console.log("On my break from class I decided to get some lunch.");
+console.log("It is " + IamHungry + " that I am hungry.");
+console.log("I only have an hour to eat so I check to see what resturants are close.");
+console.log("There are " + numOfResturantChoices + " resturants close to school.");
+console.log("I’ve never been to Crispers before so I decided to dine there because it is walking distance to class.");
+console.log("I arrive to Crispers and realize the menu is huge.")
+console.log("My waiter's name is " + myWaiterName + ".");
+console.log("They see that I'm having trouble picking from the menu.")
+askQuestion = confirm("Do you want me to help you with the menu? OK for yes, Cancel for no.");
+console.log(""+ myWaiterName + " asks me if I would like salad or soup and sandwich?")
+console.log("I ask if they have chowder and the waiter replied " + soupChoiceF + ".")
+console.log("I ask " + myWaiterName + " if the brocoli cheddar soup is good,\nthey replied " + soupChoice + ".\n");
+console.log("I choose the soup and sandwich combo.")
+console.log("My lunch was very good I would return to Crispers again!")
+
+
+
+
